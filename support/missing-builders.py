dd1412eb5f89405cffa710d92d4883d4004c4611
Tim Jenness <tjenness@lsst.org>
2018-05-13 20:51:42 -0700

    Cleverer missing authors routine
    Try to look at initials as well since we have multiple builders
    who have the same surname.

--- support/missing-builders.py
+++ support/missing-builders.py
@@ -12,9 +12,7 @@ This program requires the "yaml" package to be installed.
 """
 from __future__ import print_function
 import os
-import sys
 import os.path
-import re
 import yaml
 
 # This file currently a copy and paste from the static builders
@@ -22,10 +20,21 @@ import yaml
 BUILDERS_FILE = "builders.txt"
 
 
-def cleanSurname(surname):
+def deriveNameKey(surname, initials):
+    """Build key for indexing authors.
+    """
+    # Clean surname of latex
     for c in "'{} \\":
         surname = surname.replace(c, "")
-    return surname
+        if initials is not None:
+            initials = initials.replace(c, "")
+
+    if initials is None:
+        initials = ""
+    else:
+        initials = initials[0]
+
+    return "{}{}".format(surname, initials)
 
 
 # Paper author database
@@ -43,33 +52,53 @@ with open(infile, "r") as fh:
 authorList = authordb["authors"]
 
 authors = {}
+authors_by_surname = {}
 for auth in authorList:
     # For spaces in surnames use a ~
-    surname = cleanSurname(auth["name"])
+    nameKey = deriveNameKey(auth["name"], auth["initials"])
     author = (auth["initials"], auth["name"], auth["affil"])
-    if surname in authors:
-        authors[surname].append(author)
+    if nameKey in authors:
+        authors[nameKey].append(author)
     else:
-        authors[surname] = [author]
+        authors[nameKey] = [author]
+    nameKey = deriveNameKey(auth["name"], None)
+    if nameKey in authors_by_surname:
+        authors_by_surname[nameKey].append(author)
+    else:
+        authors_by_surname[nameKey] = [author]
 
 # Now get builder list
 builders = {}
+builders_by_surname = {}
 with open(BUILDERS_FILE, "r") as fh:
     for row in fh:
         row = row.strip()
         builder = row.split("\t")
-        surname = builder[1]
-        surname = cleanSurname(surname)
-        if surname in builders:
-            builders[surname].append(builder)
+        nameKey = deriveNameKey(builder[1], builder[0])
+        surnameKey = deriveNameKey(builder[1], None)
+        if nameKey in builders:
+            builders[nameKey].append(builder)
         else:
-            builders[surname] = [builder]
+            builders[nameKey] = [builder]
+        builders_by_surname[nameKey] = surnameKey
 
 # Now go through builders and see if they are in authors
-for builderSurname in builders:
-    if builderSurname not in authors:
-        text = str(builders[builderSurname])
-        text = text.replace("[", "")
-        text = text.replace("]", "")
+for builderKey in builders:
+    text = str(builders[builderKey])
+    text = text.replace("[", "")
+    text = text.replace("]", "")
+
+    # If the surname does not exist in authors then it is clearly missing
+    if builderKey not in authors:
         print("Missing builder: {}".format(text))
+        # Some people have different initials on the builder list.
+        # "William -> Bill" etc
+        surname = builders_by_surname[builderKey]
+        if surname in authors_by_surname:
+            print("-> Possibly present as {}".format(authors_by_surname[surname]))
         continue
+
+    # Two builders with same surname and first initial
+    if len(builders[builderKey]) > 1:
+        print("Two builders but one author. Check: {}".format(text))
+        print("-> Author list: {}".format(authors[builderKey]))
